970b51902c9a1c926722fc250c4bc1c8d277be53
Ashley Ramsey <dusty1boy1984@gmail.com>
2023-01-03 18:28:34 -0500

    Cleaned up code and corrected errors

--- src/Layout/Homepage.js
+++ src/Layout/Homepage.js
@@ -3,14 +3,14 @@ import { useHistory, Link } from "react-router-dom";
 import { listDecks, deleteDeck } from "../utils/api/index";
 
 
-function Homepage(){
+function Homepage() {
 const [decks, setDecks] = useState([]);
 const history = useHistory();
 
 
 useEffect(() => {
     async function getDeck() {
-      const response = await listDecks()
+      const response = await listDecks();
       setDecks(response);
     }
     getDeck();
@@ -34,39 +34,37 @@ useEffect(() => {
   //react key warning
 
   return (
+  <div>
+    <div>           
+      <Link to="/decks/new" className="btn btn-secondary text-white"> 
+        {" "}Create Deck
+      </Link>
+    </div>
     <div>
-        <div>           
-        <Link to="/decks/new" className="btn btn-secondary text-white"> 
-            {" "}Create Deck
-        </Link>
-        </div>
-        <div>
-            {decks?.map((deck)=>
-            <div className="d-grid gap-2 d-md-block mt-2 mb-4" key={deck}> 
-            <div className="card w-75">
-              <div className="card-header bg-light">
+      {decks?.map((deck)=>
+        <div className="d-grid gap-2 d-md-block mt-2 mb-4" key={deck}> 
+          <div className="card w-75">
+            <div className="card-header bg-light">
               <h5 className="float-left">{deck.name}</h5>
-              <p className="float-right">{deck.cards.length} cards</p>
-              </div>
-                    <div className="card-body">
-                        <p className="card-text">{deck.description}</p>
-                    </div>
-                <div className="ml-2 mr-2 mb-3">
+                <p className="float-right">{deck.cards.length} cards</p>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{deck.description}</p>
+            </div>
+            <div className="ml-2 mr-2 mb-3">
                 <Link to ={`/decks/${deck.id}`} className="btn btn-secondary float-left mr-2">
-                {" "}View
+                  {" "}View
                 </Link>                
                 <Link to={`/decks/${deck.id}/study`} className="btn btn-primary float-left">
-                {" "}Study
+                  {" "}Study
                 </Link>
-            
                 <button className="btn btn-danger float-right" onClick={() => deleteButtonHandler(deck.id)}>
                 </button>
-                </div>
-
-            </div>  
-        </div>   
-            )}  
+            </div>
+          </div>  
         </div>
+        )}  
+      </div>
     </div>
   )
 }
