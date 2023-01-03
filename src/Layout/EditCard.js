6b91a8e043eb01697dcd8c98534b1f16af20a231
Ashley Ramsey <dusty1boy1984@gmail.com>
2023-01-03 17:58:50 -0500

    Cleaned up code and corrected errors

--- src/Layout/EditCard.js
+++ src/Layout/EditCard.js
@@ -1,84 +1,80 @@
-import React, {useState, useEffect} from "react";
-import {Link, useHistory, useParams} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { readCard, readDeck, updateCard } from "../utils/api/index";
 import CardForm from "./CardForm";
 
 
 
-function EditCard(){
-const history = useHistory();
-const [card, setCard] = useState({ front: "", back: "" });
-const [deck, setDeck] = useState({})
-const {deckId, cardId} = useParams();
-
-
-const handleFrontChange = (event) => {
-    setCard({...card, front: event.target.value})
-}
-
-const handleBackChange = (event) => {
-    setCard({...card, back: event.target.value})
-}
-
-
-useEffect(() => {
-    async function loadDeck(){
-        const response = await readDeck(deckId)
-        setDeck(response)
+function EditCard() {
+    const history = useHistory();
+    const [card, setCard] = useState({ front: "", back: "" });
+    const [deck, setDeck] = useState({});
+    const {deckId, cardId} = useParams();
+    
+    const handleFrontChange = (event) => {
+        setCard({...card, front: event.target.value})
     }
-    loadDeck()
-}, [deckId])
 
-useEffect(() => {
-    async function loadCard(){
-        const response = await readCard(cardId)
-         setCard(response)
+    const handleBackChange = (event) => {
+        setCard({...card, back: event.target.value});
     }
-    loadCard()
-}, [cardId]);
-
-function cancelButtonHandler(event) {
-    event.preventDefault();
-    history.push(`/decks/${deck.id}`)
-}
-
-const submitButtonHandler = async (event) => {
-    event.preventDefault()
-    await updateCard(card)
-    history.push(`/decks/${deck.id}`)
-}
-
-
-console.log(card.front)
-
-return (
+    useEffect(() => {
+    
+        async function loadDeck() {
+            const response = await readDeck(deckId);
+            setDeck(response);
+        }
+        loadDeck();
+    }, [deckId]);
+    
+    useEffect(() => {
+        async function loadCard() {
+            const response = await readCard(cardId);
+            setCard(response);
+        }
+        loadCard();
+    }, [cardId]);
+    
+    function cancelButtonHandler(event) {
+        event.preventDefault();
+        history.push(`/decks/${deck.id}`);
+    }
+    
+    const submitButtonHandler = async (event) => {
+        event.preventDefault();
+        await updateCard(card);
+        history.push(`/decks/${deck.id}`);
+    };
+    
+    console.log(card.front)
+    
+    return (
     <div>
         <nav aria-label="breadcrumb">
-             <ol className="breadcrumb">
-                 <li className="breadcrumb-item">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item">
                      <Link to="/">
-                    Home
+                        Home
                     </Link>
                 </li>
                 <li className="breadcrumb-item">
                     <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
                 </li>
-                 <li className="breadcrumb-item">Edit Card {card.id}</li>
+                <li className="breadcrumb-item">Edit Card {card.id}</li>
             </ol>
-         </nav>
-
+        </nav>
+        
         <h2>Edit Card</h2>
         <CardForm 
-        submitButtonHandler={submitButtonHandler}
-        handleFrontChange={handleFrontChange}
-        handleBackChange={handleBackChange}
-        cancelButtonHandler={cancelButtonHandler}
-        card={card}
-        deckId={deckId}
+            submitButtonHandler={submitButtonHandler}
+            handleFrontChange={handleFrontChange}
+            handleBackChange={handleBackChange}
+            cancelButtonHandler={cancelButtonHandler}
+            card={card}
+            deckId={deckId}
         />
-
-    </div>
-)
-}
+        </div>
+        );
+    }
 
 export default EditCard;
