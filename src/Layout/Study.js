ba97dcbb2b294f0601811fce9c3cd2bb972dab44
Ashley Ramsey <dusty1boy1984@gmail.com>
2023-01-03 18:39:52 -0500

    Cleaned up code and corrected errors

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -5,81 +5,80 @@ import {readDeck} from "../utils/api/index";
 
 
 
-function Study(){
+function Study() {
     const [deck, setDeck] = useState({});
-    const [cards, setCards] = useState([])
-    const history=useHistory()
+    const [cards, setCards] = useState([]);
+    const history = useHistory();
     const {deckId} = useParams();
-    const [cardIndexNumber, setCardIndexNumber] = useState(0)
-    const [cardFrontSide, setCardFrontSide] = useState(true)
-    const [cardsLength, setCardsLength] = useState(0)
+    const [cardIndexNumber, setCardIndexNumber] = useState(0);
+    const [cardFrontSide, setCardFrontSide] = useState(true);
+    const [cardsLength, setCardsLength] = useState(0);
 
 
     useEffect(() => {
-        async function loadDeck(){
-            const response = await readDeck(deckId)
-             setDeck(response)
-             setCards(response.cards)
-             setCardsLength(response.cards.length)
+        async function loadDeck() {
+            const response = await readDeck(deckId);
+             setDeck(response);
+             setCards(response.cards);
+             setCardsLength(response.cards.length);
         }
-        loadDeck()
-    }, [deckId])
-
-     //   flip button
-        const flipButtonHandler = (event) => {
-            if (cardFrontSide === true){
-                setCardFrontSide(false)
-            } else {
-                setCardFrontSide(true)
-            }
-        }
+        loadDeck();
+    }, [deckId]);
+    
+    //   flip button
+    const flipButtonHandler = (event) => {
+        if (cardFrontSide === true) {
+            setCardFrontSide(false);
+        } else {
+            setCardFrontSide(true);
+        };
+    }
 
 
         //next button
-        const nextButtonHandler = (event) => {
-            setCardIndexNumber(cardIndexNumber + 1)
-            console.log(cardsLength, cardIndexNumber)
-            if (cardIndexNumber === cardsLength - 1){
-                if (
-                    window.confirm("Restart cards? Click 'cancel' to return to the home page.")
-                    ) {
-                        setCardIndexNumber(0);
-                        setCardFrontSide(true)
-                    } else {
-                        history.push("/")
-                    }
-            } 
-            else {
-                setCardIndexNumber(cardIndexNumber + 1);
+    const nextButtonHandler = (event) => {
+        setCardIndexNumber(cardIndexNumber + 1)
+        console.log(cardsLength, cardIndexNumber)
+        if (cardIndexNumber === cardsLength - 1) {
+            if (
+                window.confirm("Restart cards? Click 'cancel' to return to the home page.")
+            ) {
+                setCardIndexNumber(0);
                 setCardFrontSide(true)
+            } else {
+                history.push("/");
             }
+        } 
+        else {
+            setCardIndexNumber(cardIndexNumber + 1);
+            setCardFrontSide(true)
         }
-
-
-        if (cardsLength <= 2){
-            return (
-                <div>
-                    <nav aria-label="breadcrumb">
-                 <ol className="breadcrumb">
-                     <li className="breadcrumb-item">
-                    <Link to="/">
-                        Home
-                     </Link>                </li>
-                     <li className="breadcrumb-item">
+    }
+    
+    if (cardsLength <= 2) {
+        return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/">
+                            Home
+                        </Link>                </li>
+                    <li className="breadcrumb-item">
                         <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
                     </li>
-                     <li className="breadcrumb-item active" aria-current="page">Study</li>
+                    <li className="breadcrumb-item active" aria-current="page">Study</li>
                 </ol>    
             </nav>
-                    <h1>{deck.name}: Study</h1>
-                    <h3>Not enough cards.</h3>
-                    <p>You need at least 3 cards to study. There are {cardsLength} cards in this deck.</p>
-                    <Link to={`/decks/${deck.id}/cards/new`} className="btn btn-primary ml-2 float-left">
+            <h1>{deck.name}: Study</h1>
+            <h3>Not enough cards.</h3>
+            <p>You need at least 3 cards to study. There are {cardsLength} cards in this deck.</p>
+            <Link to={`/decks/${deck.id}/cards/new`} className="btn btn-primary ml-2 float-left">
                 {" "}Add Cards
-                </Link>
-                </div>
-            )
-        }
+            </Link>
+        </div>
+        )
+    }
 
 
     //need 'next' button handler
@@ -95,36 +94,30 @@ function Study(){
     return (
     <div>
         <div>
-        <nav aria-label="breadcrumb">
-           <ol className="breadcrumb">
-               <li className="breadcrumb-item">
-               <Link to="/">  
-                Home</Link>
-              </li>
-               <li className="breadcrumb-item">
-                  <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
-              </li>
-               <li className="breadcrumb-item active" aria-current="page">Study</li>
-          </ol>    
-      </nav>
-      </div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/">  
+                            Home</Link>
+                    </li>
+                    <li className="breadcrumb-item">
+                        <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">Study</li>
+                </ol>    
+            </nav>
+        </div>
         <div>
             <h1>{deck.name}: Study</h1>
         </div>
         <div className="card float-center d-grid gap-2 d-md-block mt-2 mb-4" style={{ width: '30rem' }}>
             <div className="card-body">
-            <h2>Card {cardIndexNumber+1} of {cardsLength}</h2>
-
-            <div>
-                {cardFrontSide ? <p>{cards[cardIndexNumber]?.front}</p> : <p>{cards[cardIndexNumber]?.back}</p>}
-            </div>
-
-            <button className="btn btn-secondary" onClick={flipButtonHandler}>Flip</button>
-
-           
+                <h2>Card {cardIndexNumber+1} of {cardsLength}</h2>
+                <div>
+                    {cardFrontSide ? <p>{cards[cardIndexNumber]?.front}</p> : <p>{cards[cardIndexNumber]?.back}</p>}
+                </div>
+                <button className="btn btn-secondary" onClick={flipButtonHandler}>Flip</button>
                 {cardFrontSide ? <p></p> : <button className="btn btn-primary" onClick={nextButtonHandler}>Next</button>}
-           
-
             </div>
         </div>
     </div>
